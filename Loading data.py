ef526cdbe513a9a42e33f76e0e76e831b535d955
Zhaoyu Liu <107001444+lzyqd98@users.noreply.github.com>
2023-04-16 20:12:29 -0400

    Add files via upload

---
+++ Loading data.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 17:40:38 2023
+
+@author: kaying
+"""
+
+import numpy as np
+np.save('/Users/kaying/Desktop/LWA_td.npy', LWA_td)
+# LWA_td = np.load('/Users/kaying/Desktop/LWA_td.npy')
